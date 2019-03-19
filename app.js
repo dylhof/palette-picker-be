0f5c5f1d7033a991eea49f870c051f52836969ed
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-03-19 15:26:05 -0600

    Add GET endpoint
    Co-authored-by: Dylan Hofmann <dylan.hofmann@gmail.com>

--- app.js
+++ app.js
@@ -9,4 +9,11 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
+app.get('/api/v1/projects', (request, response) => {
+  database('projects').select()
+    .then(projects => {
+      response.status(200).json(projects);
+    })
+    .catch(error => response.status(500).json({ error }));
+});
 module.exports = app;

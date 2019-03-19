a96acf139041d7c852722ef4b997826dcac1d0f2
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-03-19 08:46:03 -0600

    Add some seed data.

---
+++ paletteData.js
@@ -0,0 +1,65 @@
+module.exports = [
+  {
+    name: 'Game Time', 
+    palettes: [
+      {
+        name: 'Favorites',
+        color1: '#A0DDFF',
+        color2: '#758ECD',
+        color3: '#C1CEFE',
+        color4: '#7189FF',
+        color5: '#624CAB'
+      },
+      {
+        name: 'Not favorites',
+        color1: '#FCB97D',
+        color2: '#EDD892',
+        color3: '#C6B89E',
+        color4: '#B5B8A3',
+        color5: '#AABA9E'
+      },
+    ]
+  },
+  {
+    name: 'Movie Tracker', 
+    palettes: [
+      {
+        name: 'Lush',
+        color1: '#0EF9F9',
+        color2: '#1F81DD',
+        color3: '#6151BC',
+        color4: '#9B5094',
+        color5: '#FF5C1C'
+      },
+      {
+        name: 'Pale',
+        color1: '#BCF8EC',
+        color2: '#AED9E0',
+        color3: '#91A6E0',
+        color4: '#8B687F',
+        color5: '#7B435B'
+      },
+    ]
+  },
+  {
+    name: 'Trapper Keeper', 
+    palettes: [
+      {
+        name: 'Oceanic',
+        color1: '#EF767A',
+        color2: '#456990',
+        color3: '#49BEAA',
+        color4: '#49DCB1',
+        color5: '#EEB868'
+      },
+      {
+        name: 'Mountains',
+        color1: '#5C0029',
+        color2: '#61304B',
+        color3: '#857C8D',
+        color4: '#94BFBE',
+        color5: '#ACF7C1'
+      },
+    ]
+  },
+]
